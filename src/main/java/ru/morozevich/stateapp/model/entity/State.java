1adfc178351321699f3221627931ef2db34c1ff8
Марина <marina.morozevich@gmail.com>
2023-09-14 11:13:14 +0300

    added TestNG and created State

--- src/main/java/ru/morozevich/stateapp/model/entity/State.java
+++ src/main/java/ru/morozevich/stateapp/model/entity/State.java
@@ -1,5 +1,15 @@
 package ru.morozevich.stateapp.model.entity;
 
 public class State {
+    private static State state;
 
+    private State() {
+    }
+
+    public static State getState() {
+        if (state == null) {
+            state = new State();
+        }
+        return state;
+    }
 }
