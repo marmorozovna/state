c6e8fd45704e3ad17a97be4dadb40734bdc40ea8
Марина <marina.morozevich@gmail.com>
2023-09-15 15:40:18 +0300

    fixed Region initialization

--- src/main/java/ru/morozevich/stateapp/model/entity/State.java
+++ src/main/java/ru/morozevich/stateapp/model/entity/State.java
@@ -1,26 +1,17 @@
 package ru.morozevich.stateapp.model.entity;
 
-import java.util.EnumSet;
 import java.util.Set;
 
 public enum State {
-    STATE(Region.getAllRegions());
+    STATE();
 
-    private Set<Region> allRegions;
     private final City capital = City.CAPITAL_CITY;
 
     private State() {
-    }
-
-    private State(Set<Region> allRegions) {
-        this.allRegions = allRegions;
     }
 
     public City getCapital() {
         return this.capital;
     }
 
-    public Set<Region> getAllRegions() {
-        return this.allRegions;
-    }
 }
--- src/main/java/ru/morozevich/stateapp/model/service/CityService.java
+++ src/main/java/ru/morozevich/stateapp/model/service/CityService.java
@@ -9,59 +9,84 @@ public class CityService {
 
     public static void setRandomCity(Citizen citizen) {
         Random random = new Random();
-        switch (random.nextInt(1, 26)) {
+        switch (random.nextInt(1, 27)) {
             case 1:
                 citizen.setResidence(City.NON_CAPITAL_CITY1);
+                break;
             case 2:
                 citizen.setResidence(City.NON_CAPITAL_CITY2);
+                break;
             case 3:
                 citizen.setResidence(City.NON_CAPITAL_CITY3);
+                break;
             case 4:
                 citizen.setResidence(City.NON_CAPITAL_CITY4);
+                break;
             case 5:
                 citizen.setResidence(City.NON_CAPITAL_CITY5);
+                break;
             case 6:
                 citizen.setResidence(City.NON_CAPITAL_CITY6);
+                break;
             case 7:
                 citizen.setResidence(City.NON_CAPITAL_CITY7);
+                break;
             case 8:
                 citizen.setResidence(City.NON_CAPITAL_CITY8);
+                break;
             case 9:
                 citizen.setResidence(City.NON_CAPITAL_CITY9);
+                break;
             case 10:
                 citizen.setResidence(City.NON_CAPITAL_CITY10);
+                break;
             case 11:
                 citizen.setResidence(City.NON_CAPITAL_CITY11);
+                break;
             case 12:
                 citizen.setResidence(City.NON_CAPITAL_CITY12);
+                break;
             case 13:
                 citizen.setResidence(City.NON_CAPITAL_CITY13);
+                break;
             case 14:
                 citizen.setResidence(City.NON_CAPITAL_CITY14);
             case 15:
                 citizen.setResidence(City.NON_CAPITAL_CITY15);
+                break;
             case 16:
                 citizen.setResidence(City.NON_CAPITAL_CITY16);
+                break;
             case 17:
                 citizen.setResidence(City.NON_CAPITAL_CITY17);
+                break;
             case 18:
                 citizen.setResidence(City.NON_CAPITAL_CITY18);
+                break;
             case 19:
                 citizen.setResidence(City.NON_CAPITAL_CITY19);
+                break;
             case 20:
                 citizen.setResidence(City.NON_CAPITAL_CITY20);
+                break;
             case 21:
                 citizen.setResidence(City.NON_CAPITAL_CITY21);
+                break;
             case 22:
                 citizen.setResidence(City.NON_CAPITAL_CITY22);
+                break;
             case 23:
                 citizen.setResidence(City.NON_CAPITAL_CITY23);
+                break;
             case 24:
                 citizen.setResidence(City.NON_CAPITAL_CITY24);
+                break;
             case 25:
                 citizen.setResidence(City.NON_CAPITAL_CITY25);
+                break;
             case 26:
                 citizen.setResidence(City.CAPITAL_CITY);
+                break;
         }
     }
 }
