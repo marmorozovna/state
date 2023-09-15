241e3473e7d6e797e9e5cc3583655e108f19f809
Марина <marina.morozevich@gmail.com>
2023-09-15 11:35:53 +0300

    created createName and createAge

---
+++ src/main/java/ru/morozevich/stateapp/model/service/CitizenCreator.java
@@ -0,0 +1,25 @@
+package ru.morozevich.stateapp.model.service;
+
+import ru.morozevich.stateapp.model.entity.Citizen;
+
+import java.util.Random;
+
+public class CitizenCreator {
+    public String createName() {
+        int leftLimitChar = 97;
+        int rightLimitChar = 122;
+        int targetStringLength = (int) (Math.random() * 10 + 5);
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimitChar +
+                    (int) (random.nextFloat() * (rightLimitChar - leftLimitChar + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        return buffer.toString();
+    }
+
+    public int createAge() {
+        return (int) (Math.random() * 100);
+    }
+}
