f58fc41afab8728c31f26be00c5e81ef6f1da3d5
marmorozovna <marina.morozevich@gmail.com>
2023-09-20 11:43:08 +0300

    created name

--- src/main/java/ru/morozevich/stateapp/model/entity/District.java
+++ src/main/java/ru/morozevich/stateapp/model/entity/District.java
@@ -2,22 +2,24 @@ package ru.morozevich.stateapp.model.entity;
 
 public enum District {
 
-    CENTAL_DISTRICT(Region.CAPITAL_REGION, true),
+    CENTAL_DISTRICT(Region.CAPITAL_REGION, true, "Центральный район"),
     FINANICIAL_DISTRICT(Region.CAPITAL_REGION, false),
     CULTURAL_DISTRICT(Region.CAPITAL_REGION, false),
     UPSTREAM_DISTRICT(Region.INDUSTRIAL_REGION, false),
     DOWNSTREAM_DISTRICT(Region.INDUSTRIAL_REGION, false),
-    MANAGEMENT_DISTRICT(Region.INDUSTRIAL_REGION, true),
+    MANAGEMENT_DISTRICT(Region.INDUSTRIAL_REGION, true, "Управленческий район"),
     PLANT_GROWING_DISTRICT(Region.RURAL_REGION, false),
     PASTORAL_DISTRICT(Region.RURAL_REGION, false),
-    MARKET_DISTRICT(Region.RURAL_REGION, true),
-    LIVING_DISTRICT(Region.ANCIENT_REGION, true),
+    MARKET_DISTRICT(Region.RURAL_REGION, true, "Рыночный район"),
+    LIVING_DISTRICT(Region.ANCIENT_REGION, true, "Жилой район"),
     TOURISTIC_DISTRICT(Region.ANCIENT_REGION, false),
     BEACH_DISTRICT(Region.SEESIDE_REGION, false),
-    NORTH_DISTRICT(Region.SEESIDE_REGION, true);
+    NORTH_DISTRICT(Region.SEESIDE_REGION, true, "Северный район");
 
     private Region region;
     private boolean isRegionCenter;
+
+    private String name;
 
     private District() {
     }
@@ -31,11 +33,21 @@ public enum District {
         this.isRegionCenter = isRegionCenter;
     }
 
+    private District(Region region, boolean isRegionCenter, String name) {
+        this(region, isRegionCenter);
+        this.name = name;
+    }
+
     public Region getRegion() {
         return this.region;
     }
 
     public boolean isRegionCenter() {
         return isRegionCenter;
+    }
+
+    @Override
+    public String toString(){
+        return this.name;
     }
 }
