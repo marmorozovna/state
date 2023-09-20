6c5881c2e7c2ae82f2dd9bbb41999ac083e79450
marmorozovna <marina.morozevich@gmail.com>
2023-09-20 11:15:59 +0300

    fixed testGetAverageAge

--- src/test/java/ru/morozevich/stateapp/model/service/TestState.java
+++ src/test/java/ru/morozevich/stateapp/model/service/TestState.java
@@ -5,23 +5,26 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import ru.morozevich.stateapp.model.entity.Citizen;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 
 
 public class TestState {
 
-    @BeforeTest()
-    public void testSetup() {
+    @Test()
+    public void testGetAverageAge() {
         Citizen citizen1 = new Citizen(89, "maichdlsn");
         Citizen citizen2 = new Citizen(45, "ofbwc");
         Citizen citizen3 = new Citizen(20, "fwmbjgd");
         Citizen citizen4 = new Citizen(11, "msdfjkdlv");
         Citizen citizen5 = new Citizen(79, "aichdlsnpdirgdp");
-    }
-    @Test()
-    public void testGetAverageAge() {
-        double actual = CitizenService.getAverageAge();
+        HashSet<Citizen> stateCitizen = new HashSet<>();
+        stateCitizen.add(citizen1);
+        stateCitizen.add(citizen2);
+        stateCitizen.add(citizen3);
+        stateCitizen.add(citizen4);
+        stateCitizen.add(citizen5);
+        double actual = CitizenService.getAverageAge(stateCitizen);
         double expected = 48.8;
-        Assert.assertEquals(actual,expected);
+        Assert.assertEquals(actual, expected);
     }
 }
