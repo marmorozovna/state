15378c2092aea56c9e713da85fd3ae7eca191691
marmorozovna <marina.morozevich@gmail.com>
2023-09-20 11:38:12 +0300

    created Controller

--- src/main/java/ru/morozevich/stateapp/_main/_Main.java
+++ src/main/java/ru/morozevich/stateapp/_main/_Main.java
@@ -1,6 +1,10 @@
 package ru.morozevich.stateapp._main;
 
+import ru.morozevich.stateapp.controller.Controller;
+
 public class _Main {
     public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.startApp();
     }
 }
